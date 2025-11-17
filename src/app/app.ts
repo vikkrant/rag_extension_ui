c8a19a520c80d1b54bb1640d8a4220aa1cff9abd
Vikrant Talwar <vikrant1463@gmail.com>
2025-11-17 17:02:07 -0500

    commit_2

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,38 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('iflows-intelligence');
+
+  showTextbox = false;
+  userInput = '';
+  response = "Generating Response...";
+
+  onSquare1Click() {
+    this.showTextbox = true;
+  }
+
+  enterTextbox(value: string) {
+    this.userInput = value;
+    if (this.userInput == "" || this.userInput == " ") {
+      this.userInput = "Hello Assistant!"
+    }
+    const payload = { message: this.userInput };
+
+    fetch('http://127.0.0.1:8000/send_request', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.response = data.response;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+    
+    
+  }
+
 }
